110a99630021705cd8ed28fe2fb9d682798bfb0e
aaanjini <zini6633@gmail.com>
2022-01-19 17:01:13 +0900

    하샤드 수
    프로그래머스 12947

--- Programmers/12947.js
+++ Programmers/12947.js
@@ -1,16 +1,15 @@
 function solution(x) {
     let answer = true;    
     let num = 0;
-    let str = x.toString();
-    let str_split = str.split('');
+    let str = x.toString().split('');
 
-    for(let i=0; i<str_split.length; i++){
-        num += parseInt(str_split[i]);          
-    }
+    for(let i=0; i<str.length; i++){
+        num += parseInt(str[i]);          
+    };
     if(x % num == 0){
-        return answer = true
+        return answer = true;
     }else{
-        return answer = false
-    }
+        return answer = false;
+    };
     
 }

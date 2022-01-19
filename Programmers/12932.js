b73d803d22f387bbef3dcd6a77eab62c0238e871
aaanjini <zini6633@gmail.com>
2022-01-19 16:01:30 +0900

    자연수 뒤집어 배열로 만들기
    프로그래머스 12932

--- Programmers/12932.js
+++ Programmers/12932.js
@@ -1,13 +1,11 @@
 function solution(n) {
     let answer = [];
-    const str = n.toString();
-    const str_split = str.split('');    
+    let num = n.toString().split(''); //숫자를 문자열로 만든 뒤 자리마다 쪼갭니다.
+    let arr = [];
     
-    for(let i = str_split.length-1; i >= 0; i--){
-        const aaaa = parseInt(str_split[i]); 
-        answer.push(aaaa)
-    }
+    for(let i = num.length-1; i >= 0; i--){ //num의 길이에서 1뺀값 즉 끝에서 부터 시작해서 -1씩 뺍니다. (5,4,3...)
+        arr.push(parseInt(num[i]));   // num[i]를 정수로 변환해줍니다.
+    };
     
-    
-    return answer;
+    return arr;
 }

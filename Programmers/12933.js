f731d521e7a4d3c094d3c44293612099ca764533
aaanjini <zini6633@gmail.com>
2022-01-19 16:23:00 +0900

    정수 내림차순으로 배치하기
    프로그래머스 12933

--- Programmers/12933.js
+++ Programmers/12933.js
@@ -1,17 +1,7 @@
 function solution(n) {
-    let answer = 0;
-    const str = n.toString();
-    const str_split = str.split('');    
-    
-    str_split.sort(function(a, b)  {
-      return b - a;
-    });
-    
-    let aa = str_split.join('');
-            
-    answer = parseInt(aa)
-    
-    
-    
-    return answer;
+  let num = n.toString().split('');
+  let sort = num.sort((a,b) => b - a);    
+  let answer = sort.join('');
+  
+  return parseInt(answer);
 }

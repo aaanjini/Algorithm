3870e2de63b8c49096c5be87fcdc8860bf9e8830
aaanjini <zini6633@gmail.com>
2022-01-19 16:59:09 +0900

    콜라츠 추측
    프로그래머스 12943

--- Programmers/12943.js
+++ Programmers/12943.js
@@ -3,28 +3,28 @@ function solution(num) {
     
     if(num == 1){
         return 0;
-    }
+    };
     
     for(let i = 0; i < 500; i++){
         
         if(num%2 == 0){
-            num = num/2
-            count++
+            num = num/2;
+            count++;
             if(num == 1){
-                return count
-            }
+                return count;
+            };
         }else{
-            num = num*3+1
-            count++            
+            num = num*3+1;
+            count++;
             if(num == 1){
-                return count
-            }
-        }
+                return count;
+            };
+        };
         if (count == 500 && num != 1){
-            return -1
-        }
+            return -1;
+        };
         
         
-    }
+    };
 
 }

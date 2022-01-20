2ea4a466c4ee15c1bc1fea5b6925a39be0f68992
aaanjini <zini6633@gmail.com>
2022-01-20 10:55:51 +0900

    제일 작은 수 제거하기
    프로그래머스 12935

--- Programmers/12935.js
+++ Programmers/12935.js
@@ -1,7 +1,9 @@
 function solution(arr) {
     arr.splice(arr.indexOf(Math.min(...arr)),1);
-    if(arr.length<1){
+    
+    if(arr.length === 0){
         return[-1]
     };
+    
     return arr;
 }

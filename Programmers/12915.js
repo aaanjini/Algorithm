7664b429fa08d7aef8862d2392f30cfbb0877393
aaanjini <zini6633@gmail.com>
2022-01-20 14:51:48 +0900

    문자열 내 마음대로 정렬하기
    프로그래머스 12915

--- Programmers/12915.js
+++ Programmers/12915.js
@@ -1,4 +1,5 @@
 function solution(strings, n) {
     let answer = [];
     return strings.sort().sort((a,b) => a.charCodeAt(n)-b.charCodeAt(n));
+    //charCodeAt : 주어진 인덱스에 대한 UTF-16 코드를 나타내는 0부터 65535 사이의 정수를 반환합니다. (사전순으로 정렬)
 }

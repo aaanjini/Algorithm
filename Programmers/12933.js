4b4e4e720ed41c0afdf24686c8ac31406e2946e5
aaanjini <zini6633@gmail.com>
2022-01-20 11:47:33 +0900

    정수 내림차순으로 배치하기
    프로그래머스 12933

--- Programmers/12933.js
+++ Programmers/12933.js
@@ -1,7 +1,9 @@
 function solution(n) {
-  let num = n.toString().split('');
-  let sort = num.sort((a,b) => b - a);    
-  let answer = sort.join('');
+  let answer = 0;
+  let sort = n.toString().split('');//n을 문자열로 만든 뒤 잘라서 배열로 만듭니다.
   
-  return parseInt(answer);
+  sort.sort((a,b) => b - a ); //내림차순으로 정렬합니다. 
+  sort = sort.join('');//합친 값을 sort에 담아줍니다.
+  
+  return parseInt(sort); //sort를 정수로 변환한 값을 리턴합니다.
 }

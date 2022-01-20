148470c8ba2d9a415ae1bad21ef80df7b2ca7414
aaanjini <zini6633@gmail.com>
2022-01-20 13:11:15 +0900

    완주하지 못한 선수
    프로그래머스 42576

--- Programmers/42576.js
+++ Programmers/42576.js
@@ -6,6 +6,7 @@ function solution(participant, completion) {
     
     for(let i = 0; i < participant.length; i++){        
         if(completion[i] !== participant[i]){ //두 배열을 돌면서 완주자와 비완주자가 일치하지 않을 때
+            //완주한 선수배열보다 완주하지못한 선수배열의 길이가 무조건 1크므로 비교가능
             return participant[i] // 그 값을 리턴합니다.
         };
     };

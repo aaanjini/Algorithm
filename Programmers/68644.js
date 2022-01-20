eb0a0d67fa16c269abfa89d9cdafe1a758ec4f09
aaanjini <zini6633@gmail.com>
2022-01-20 13:25:45 +0900

    두 개 뽑아서 더하기
    프로그래머스 68644

--- Programmers/68644.js
+++ Programmers/68644.js
@@ -1,16 +1,16 @@
 function solution(numbers) {
-    var answer = [];
+    let answer = [];
     
     for ( let i = 0; i < numbers.length; i++) {
-      for ( let j = i + 1; j < numbers.length; j++) {
+      for ( let j = i + 1; j < numbers.length; j++) {//number[i]번째요소의 다음 요소를 가르킴 ex) n= [1,2,3] 여기서 [i]는 1, [j]는 2,3
         let sum = numbers[i] + numbers[j];
-        if( answer.indexOf(sum) === -1) {
-          answer.push(sum);
+        if( answer.indexOf(sum) === -1) {//배열에서 sum을 찾았을 때 없을 경우에만
+          answer.push(sum);//배열에 추가
         }
       }
     }
     
-    answer.sort((a,b) => a - b);
+    answer.sort((a,b) => a - b);//오름차순으로 정렬
     
     return answer;
 }

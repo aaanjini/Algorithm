4f09b089cac5b639fcef62888466046d6be28a51
aaanjini <zini6633@gmail.com>
2022-01-20 11:41:03 +0900

    정수 제곱근 판별
    프로그래머스 12934

--- Programmers/12934.js
+++ Programmers/12934.js
@@ -1,10 +1,10 @@
 function solution(n) {
-    let answer = 0;    
-    let num = Math.sqrt(n)  
+    let answer = 0;
+    let x = Math.sqrt(n);//n의 제곱근을 x에 담습니다.
     
-    if(Number.isInteger(num) == false){
-        return -1;
+    if(Number.isInteger(x) == false){//x가 정수인지 판별합니다.
+        return -1;//아니라면 -1 리턴
     }else{
-        return Math.pow(num+1,2);
+        return Math.pow(x+1,2);//맞다면 x+1의 제곱을 리턴합니다.
     }
 }

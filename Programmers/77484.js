399842fbc1a4a82288d5eff8660a001b738a80f2
aaanjini <zini6633@gmail.com>
2022-01-20 14:42:31 +0900

    로또의 최고 순위와 최저 순위
    프로그래머스 77484

--- Programmers/77484.js
+++ Programmers/77484.js
@@ -1,13 +1,11 @@
 function solution(lottos, win_nums) {
     let answer = [];
-    const zero = lottos.filter(v=>!v).length;
-    const lotto = lottos.filter((el,index)=> win_nums.includes(el)).length;
-    const max = lotto + zero;
+    const zero = lottos.filter(v=>!v).length; //로또 번호 중에서 0의 갯수를 담는다.
+    const lotto = lottos.filter((el,index)=> win_nums.includes(el)).length;//lottos에 win_num와 같은 번호가 몇개있는지
+    const max = lotto + zero; //0의 갯수와 맞은 번호의 갯수를 더합니다.
     
-    console.log(zero,lottos,max);
-    
-    if (lotto>1){
-       answer = [7-max,7-lotto]
+    if (lotto>1){ //맞은 갯수가 1개 이상이라면
+       answer = [7-max,7-lotto] 
     } else{
         if(max>0 ){
             answer = [7-max,6]    

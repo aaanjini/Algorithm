df87b6d29d6bcabc26f207a51e7c4bd5cdab8ffd
aaanjini <zini6633@gmail.com>
2022-01-20 13:17:08 +0900

    같은 숫자는 싫어
    프로그래머스 12906

--- Programmers/12906.js
+++ Programmers/12906.js
@@ -1,10 +1,9 @@
 function solution(arr){
-  let a = [];
-  for(let i = 0; i < arr.length; i++){
-    if( arr[i] !== arr[i+1]){
-      a.push(arr[i])
-    }
-  };
-  console.log(a)
-  return a;
+    let answer = [];
+    for(let i = 0; i < arr.length; i++){
+        if( arr[i] !== arr[i+1]){//arr[i]와 다음숫자인 arr[i+1]이 같지않을경우에만
+          answer.push(arr[i])//배열에 요소를 넣는다.
+        }
+    };
+    return answer;
 }

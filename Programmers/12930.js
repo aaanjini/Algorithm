a2de44d49289353b4e7369028e1cd0f50cbff43b
aaanjini <zini6633@gmail.com>
2022-01-20 13:09:15 +0900

    이상한 문자 만들기
    프로그래머스 12930

--- Programmers/12930.js
+++ Programmers/12930.js
@@ -1,20 +1,17 @@
 function solution(s) {
-    let answer = '';    
-    let arr = s.split(' '); 
+    let answer = '';
+    let arr = s.split(' '); //s를 공백을 기준으로 잘라 배열로 만든다.
     
-    for(let j=0; j < arr.length; j++){        
-        for(let i=0; i < arr[j].length; i++){           
-            if(i % 2 == 0){
-                answer+=arr[j][i].toUpperCase();       
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr[i].length; j++){
+            if(j % 2 === 0){ // j를 2로 나눈 값이 0일 경우 (짝수일경우)
+                answer += arr[i][j].toUpperCase(); // arr의 [i]번째 (ex try)의 [j](ex.t)를 대문자로 변환   
             }else{
-                answer+=arr[j][i].toLowerCase();
-            }  
-        }  
-        answer+=' ';
+                answer += arr[i][j].toLowerCase();// arr의 [i]번째 (ex try)의 [j](ex.t)를 소문자로 변환   
+            }                      
+        }
+        answer += " "; //바깥 for문이 한 번 돌때마다 공백 추가 (try공백추가hello공백추가world공백추가)
     }
     
-    
-        
-    console.log(answer)
-    return answer.slice(0, -1);
+    return answer.slice(0,-1); //마지막에 남는 공백 제거 후 리턴
 }

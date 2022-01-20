c67a447825c4d5df5a7eb70b9d8627fa02719233
aaanjini <zini6633@gmail.com>
2022-01-20 15:02:31 +0900

    소수만들기
    프로그래머스 12977

--- Programmers/12977.js
+++ Programmers/12977.js
@@ -1,11 +1,11 @@
-function check(sum){
+function check(sum){ //소수 판별 함수
     for(let i = 2; i*i <= sum; i++){
-      if(sum % i === 0) return false;
+      if(sum % i === 0) { // 2로 나뉘면 소수가 아니므로 return false
+          return false;
+      }
     }
-  
     return true;
-  }
-  
+  }  
   
   function solution(nums) {
       let count = 0;
@@ -15,11 +15,11 @@ function check(sum){
              for(let c = b+1; c < nums.length; c++){
                   //console.log(sum);
                   if(check(nums[a] + nums[b] + nums[c])){
-                      count++
+                    //a,b,c 합친 값을 check함수로 넘겨 트루라면 카운트 증가
+                    count++
                   }
               } 
           }
       }
-      //console.log(count)
       return count;
   }

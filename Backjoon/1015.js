9fb78785f89b18cae212cf37253b142b6fb205cb
aaanjini <zini6633@gmail.com>
2022-02-24 15:52:23 +0900

    백준 1015 수열정렬

---
+++ Backjoon/1015.js
@@ -0,0 +1,18 @@
+let fs = require('fs');
+const [n, ...num] = require('fs').readFileSync('/dev/stdin').toString().trim().split(/\s+/);
+
+function solution(n, num){
+    let answer = [];
+    const arr =  num;
+    const arr2 =  arr.slice();
+    const sort = arr2.sort((a,b) => a-b);
+    
+
+    for(let i = 0; i < n; i++){
+        answer.push(arr.indexOf(sort[i]));
+      
+    }
+    return answer;
+}
+
+console.log(solution(n,num));

2fd3d0629f1e33d59dcd75a105ed8047b8539767
aaanjini <zini6633@gmail.com>
2022-03-03 10:11:57 +0900

    백준 1929 소수만들기

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
-const fs = require('fs');
-const stdin = (process.platform === 'linux'
-    ? fs.readFileSync('/dev/stdin').toString()
-    : 
-`2
-6 12 1
-30 50 72`
-).split('\n');
+const input = require('fs').readFileSync('example.txt').toString().trim().map(v => +v);;
 
-const input = (() => {
-    let line = 0;
-    return () => stdin[line++];
-})();
+let M = input[0];
+let N = input[1];
 
-let line = input();
 
-while(line){
-    let temp = input().split(' ').map(v => +v);
-    let H = temp[0]; //호텔의 층 수
-    let W = temp[1]; //각 층의 방 수
-    let N = temp[2]; //몇 번째 손님
-    let count = 1; //거리
-
-    while(N > H){
-        count ++;
-        N = N - H;
+const is_prime = (num) => {
+    if (num === 1) {
+        return false;
     }
-    if(count < 10){
-        console.log(`${N}0${count}`);
-    }else{
-        console.log(`${N}${count}`);
+    for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
+        if (num % i === 0) {
+        return false;
+        }
     }
+    return true;
+}; 
 
-    line -= 1;
-};
+for(let i = M; i <= N; i++ ){
+    is_prime(i)?console.log(i):"";
+}
+
+
+
 
 
 
